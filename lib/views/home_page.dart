15380ab94ff7c664bf3b939956b8ae46ef5a6b84
Gerson Maus <gersonmaus@sou.faccat.br>
2024-10-29 09:16:39 -0300

    Ultimo commit da branch bkp-eco

---
+++ lib/views/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:ecointerior/widgets/custom_popupmenu_widget.dart';
+import 'package:ecointerior/widgets/custom_titulo_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: CustomPopupmenuWidget()),
+              Image.asset(
+                'lib/utils/images/logo_EcoInterior.png',
+                height: 210,
+              ),
+              const SizedBox(
+                height: 26,
+              ),
+              CustomTituloWidget(titulo: "| Home Page |")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
